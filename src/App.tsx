25c062004e3235023e3633a02ea814e33cc2f4d9
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-08-10 23:56:12 +0200

    list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.scss";
 
 const App: React.FC = () => {
-	return <div className={styles.App}></div>;
+	const [monsters, setMonsters] = useState([1, 2, 3]);
+	return (
+		<div className={styles.App}>
+			{monsters.map((m, i) => (
+				<h1 key={i}>{m}</h1>
+			))}
+		</div>
+	);
 };
 
 export default App;
