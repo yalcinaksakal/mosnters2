5c8e2fe4da4db94f6bdfe44503c42090872da382
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-08-11 02:05:15 +0200

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.scss";
+type user = {
+	id: number;
+	name: string;
+	[key: string]: string | { [key: string]: string } | number;
+};
 
 const App: React.FC = () => {
-	const [monsters, setMonsters] = useState([1, 2, 3]);
+	const [monsters, setMonsters] = useState<user[]>([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(
+					"https://jsonplaceholder.typicode.com/users"
+				),
+				data = await response.json();
+			setMonsters(data);
+		};
+		fetchData();
+	}, []);
 	return (
 		<div className={styles.App}>
 			{monsters.map((m, i) => (
-				<h1 key={i}>{m}</h1>
+				<h1 key={m.id}>{m.name}</h1>
 			))}
 		</div>
 	);
