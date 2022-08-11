3310905ccc003db7d722b9d91519b52035825995
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-08-11 02:44:32 +0200

    search

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,39 @@ type user = {
 };
 
 const App: React.FC = () => {
-	const [monsters, setMonsters] = useState<user[]>([]);
+	const [monsters, setMonsters] = useState<user[]>([]),
+		[filtered, setFiltered] = useState<user[]>([]),
+		[canSearch, setCanSearch] = useState(false);
+	console.log("render");
 	useEffect(() => {
+		console.log("mounted");
 		const fetchData = async () => {
 			const response = await fetch(
 					"https://jsonplaceholder.typicode.com/users"
 				),
 				data = await response.json();
 			setMonsters(data);
+			setFiltered(data);
+			setCanSearch(true);
 		};
 		fetchData();
 	}, []);
 	return (
 		<div className={styles.App}>
-			{monsters.map((m, i) => (
+			<input
+				className={styles.Search}
+				placeholder="Search monsters"
+				type="search"
+				onChange={e => {
+					const searchStr = e.target.value.toLocaleLowerCase();
+					setFiltered(
+						monsters.filter(m => m.name.toLocaleLowerCase().includes(searchStr))
+					);
+				}}
+				autoFocus
+				disabled={!canSearch}
+			/>
+			{filtered.map((m, i) => (
 				<h1 key={m.id}>{m.name}</h1>
 			))}
 		</div>
