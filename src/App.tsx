84c65a81363853ff107f23f30297f802c03ebbab
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-08-11 18:20:37 +0200

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import styles from "./App.module.scss";
 import CardList from "./components/Card-list/Card-list";
 import SearchBox from "./components/Search-box/Search-box";
@@ -7,6 +7,7 @@ const App: React.FC = () => {
 	const [monsters, setMonsters] = useState<user[]>([]),
 		[filtered, setFiltered] = useState<user[]>([]),
 		[canSearch, setCanSearch] = useState(false),
+		hasFetchedData = useRef(false),
 		filterMonsters: (e: ChangeEvent<HTMLInputElement>) => void = e => {
 			const searchStr = e.currentTarget.value.toLocaleLowerCase();
 			setFiltered(
@@ -16,16 +17,22 @@ const App: React.FC = () => {
 	console.log("render");
 	useEffect(() => {
 		console.log("mounted");
+
 		const fetchData = async () => {
+			console.log("fetching");
 			const response = await fetch(
 					"https://jsonplaceholder.typicode.com/users"
 				),
 				data = await response.json();
+			console.log(data);
 			setMonsters(data);
 			setFiltered(data);
 			setCanSearch(true);
 		};
-		fetchData();
+		if (!hasFetchedData.current) {
+			hasFetchedData.current = true;
+			fetchData();
+		}
 	}, []);
 	return (
 		<div className={styles.App}>
